a548324b65cd7fbbefacbff6763e4be7e9e524c3
Aimee Amankwah <amankwahaimee@gmail.com>
2025-08-31 00:08:25 +0100

    EDIT SETTING.PY 2

--- ecorecipes/settings.py
+++ ecorecipes/settings.py
@@ -94,7 +94,8 @@ DATABASES = {
     }
 }
 
-DATABASES["default"] = dj_database_url.parse("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(
+    "postgresql://ecorecipesdatabase_user:Tulhr4xALW4OuomUMk9SXJqkKaC1b6P5@dpg-d2n5pjp5pdvs73cj8pd0-a/ecorecipesdatabase")
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
