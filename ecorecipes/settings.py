5eaa191fd8d7436deba313af41dd8a1bebf93ea6
Aimee Amankwah <amankwahaimee@gmail.com>
2025-09-01 11:52:59 +0100

    added if database_url:

--- ecorecipes/settings.py
+++ ecorecipes/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
 }
 
 database_url = os.environ.get("DATABASE_URL")
-DATABASES["default"] = dj_database_url.parse(database_url)
-print("DEBUG DATABASE_URL:", os.getenv("DATABASE_URL"))
+if database_url:
+    DATABASES["default"] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
