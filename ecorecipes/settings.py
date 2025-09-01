79f709b391ce408f00a8c951d0bc03f097552437
Aimee Amankwah <amankwahaimee@gmail.com>
2025-09-01 10:17:19 +0100

    debug variable changed

--- ecorecipes/settings.py
+++ ecorecipes/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p$p-i&swdp37mfnic@-my53t4@s1en-h01op5^23y9945ig$o!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower == "true"
 
 ALLOWED_HOSTS = ['*']
 
