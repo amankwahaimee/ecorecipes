bfba66ffe075ba4a9c1f660d79f38ceaac283a92
Aimee Amankwah <amankwahaimee@gmail.com>
2025-09-01 11:40:47 +0100

    print databse type

--- ecorecipes/settings.py
+++ ecorecipes/settings.py
@@ -95,6 +95,7 @@ DATABASES = {
 
 database_url = os.environ.get("DATABASE_URL")
 DATABASES["default"] = dj_database_url.parse(database_url)
+print("DEBUG DATABASE_URL:", os.getenv("DATABASE_URL"))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
